d10a52e8bb05ff6a947ec1c412761beb49d45323
michaworku <mikias27worku@gmail.com>
2022-03-16 15:18:32 +0300

    Home page ui

--- react-admin/src/pages/Home/Home.tsx
+++ react-admin/src/pages/Home/Home.tsx
@@ -1,9 +1,16 @@
 import { FC } from 'react'
+import { Navbar, Sidebar } from '../../components'
 import './home.scss'
 
 const Home : FC = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <Sidebar/>
+      <div className="homeContainer">
+        <Navbar />
+        home container
+      </div>
+    </div>
   )
 }
 
