061da1029cc07a884919a064360909721c1fcd51
michaworku <mikias27worku@gmail.com>
2022-03-16 12:36:48 +0300

    configuring routes

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -1,7 +1,30 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { Home, List, Login, New, Single } from "./pages";
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+         <Route path ='/'>
+           <Route index element={<Home/>}/>
+           <Route path = 'login' element = {<Login/>}/>
+           <Route path = 'users'>
+             <Route index element={<List/>}/>
+             <Route path=':userId' element={<Single/>}/>
+             <Route path = 'new' element = {<New/>}/>
+           </Route>
+           <Route path = 'products'>
+             <Route index element={<List/>}/>
+             <Route path=':productId' element={<Single/>}/>
+             <Route path = 'new' element = {<New/>}/>
+           </Route>
+         </Route>
+      </Routes>
+    </Router>
   )
 }
 
