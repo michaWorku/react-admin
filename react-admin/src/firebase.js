4395f6757ece790bbe880dcea59ad10acb8b3496
michaworku <mikias27worku@gmail.com>
2022-03-31 16:32:11 +0300

    add firebase to app

---
+++ react-admin/src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBfa0gIgd0kIPVAqp-52vaxSLrbbcOolEc",
+  authDomain: "react-admin-1216192127.firebaseapp.com",
+  projectId: "react-admin-1216192127",
+  storageBucket: "react-admin-1216192127.appspot.com",
+  messagingSenderId: "1084685797404",
+  appId: "1:1084685797404:web:9c48870644f1e580a7b7d9"
+};
+
+const app = initializeApp(firebaseConfig);
